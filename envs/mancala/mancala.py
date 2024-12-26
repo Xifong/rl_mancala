41d153fca8887d57e2188cb330394febce562df1
Xifong Christian <xifongchristian@gmail.com>
2024-12-26 22:27:27 +0000

    updated comment

--- envs/mancala/mancala.py
+++ envs/mancala/mancala.py
@@ -5,11 +5,11 @@ import itertools as it
 
 
 def random_opponent_policy(seed: int, observation: list[int]) -> int:
+    # Depends on the opponent side being the last (but one) six elements of the observation
     opponent_side = observation[-7:-1]
     assert sum(opponent_side) > 0, "Opponent has no valid moves"
 
     random_action = np.random.randint(0, 6)
-    # Depends on the player side being the first six elements of the observation
     while opponent_side[random_action] == 0:
         random_action = np.random.randint(0, 6)
 
